b7e6fcb29cbb8a7396c10706de46d016f4d237d8
tolyaganzin <tolyaganzin@gmail.com>
2026-08-15 12:54:13 +0300

    Update MyCars with select form

--- src/components/MyCars.jsx
+++ src/components/MyCars.jsx
@@ -1,13 +1,34 @@
+import { useState } from 'react'
 const MyCars = () => {
   const cars = ['Ford', 'BMW', 'Audi'];
+
+  const [myCar, setMyCar] = useState("Volvo");
+
+  const handleChange = (event) => {
+    setMyCar(event.target.value)
+  }
+
   return (
     <>
       <h3>My Cars:</h3>
-      <ul>
-        {cars.map((car, index) => <li key={index}>I am a { car }</li>)}
-      </ul>
+      <form>
+        <span>selected car: </span>
+        <select value={myCar} onChange={handleChange}>
+          {cars.map((car) => <option  value={car}>{car}</option>)}
+        </select>
+      </form>
+      <ul>{cars.map((car, index) => <li key={index}>I am a { car }</li>)}</ul>
+
     </>
-  );
+  )
+  // return (
+  //   <>
+  //     <h3>My Cars:</h3>
+  //     <ul>
+  //       {cars.map((car, index) => <li key={index}>I am a { car }</li>)}
+  //     </ul>
+  //   </>
+  // );
 }
 
 export default MyCars;
