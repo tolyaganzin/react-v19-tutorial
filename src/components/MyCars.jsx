856acc1983d49bbfcc14d02c520146406a54dbb6
tolyaganzin <tolyaganzin@gmail.com>
2026-08-15 12:21:53 +0300

    add MyCars component

---
+++ src/components/MyCars.jsx
@@ -0,0 +1,13 @@
+function MyCars() {
+  const cars = ['Ford', 'BMW', 'Audi'];
+  return (
+    <>
+      <h3>My Cars:</h3>
+      <ul>
+        {cars.map((car, index) => <li key={index}>I am a { car }</li>)}
+      </ul>
+    </>
+  );
+}
+
+export default MyCars;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import MyCars from './components/MyCars.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <MyCars />
   </StrictMode>,
 )
